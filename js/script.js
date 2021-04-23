41b42c0fee35058406f47a07c87172cc8bc170d9
Dmitriy Van der Elst <dmitriy.vde@gmail.com>
2021-04-23 14:26:37 +0200

    Added quizitems and welcome screen functionality

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
     window.addEventListener('load', function() {
         console.log('Ready')
 
+        // Default state if no saved state is found
         let defaultState = {
             startupWindow: 'welcomeWindow',
             activeWindow: '',
@@ -14,6 +15,7 @@
             }
         }
 
+        // Current active state
         let state = {
             startupWindow: 'welcomeWindow',
             activeWindow: '',
@@ -24,6 +26,7 @@
             }
         }
 
+        // All developer elements and their selectors
         const devElements = {
             devWelcome: document.getElementById('dev__welcome'),
             devMenu: document.getElementById('dev__menu'),
@@ -31,12 +34,14 @@
             devReset: document.getElementById('dev__reset')
         }
 
+        // All windows and their selectors
         const windows = {
             welcomeWindow: document.getElementById('main__welcome'),
             menuWindow: document.getElementById('main__menu'),
             gameWindow: document.getElementById('main__game')
         }
 
+        // All elements that should be addressable
         const allElements = {
             welcome: {
                 nicknameText: document.getElementById('welcome__nickname'),
@@ -50,6 +55,7 @@
             }
         }
 
+        // Set the active window
         function setActiveWindow(windowName) {
             windows.welcomeWindow.style.display = 'none'
             windows.menuWindow.style.display = 'none'
@@ -58,6 +64,7 @@
             state.activeWindow = windowName
         }
 
+        // Saves the current state to local storage
         function saveState() {
             localStorage.setItem('savedState', JSON.stringify(state))
             console.log('State saved')
@@ -70,8 +77,7 @@
         localStorage.setItem('savedState', JSON.stringify(savedState))
         state = JSON.parse(localStorage.getItem('savedState'))
 
-        console.log(state)
-
+        // Resets the local storage - Dev only
         function resetLocalStorage() {
             localStorage.removeItem('savedState')
             setActiveWindow('welcomeWindow')
@@ -80,6 +86,8 @@
             console.log('State reset')
         }
         
+
+        // Developer functions
         devElements.devReset.addEventListener('click', resetLocalStorage)
 
         setActiveWindow(state.startupWindow)
@@ -88,6 +96,8 @@
         devElements.devMenu.addEventListener('click', function() {setActiveWindow('menuWindow')})
         devElements.devGame.addEventListener('click', function() {setActiveWindow('gameWindow')})
 
+        // WELCOME SECTION
+        // Set nickname on welcome screen
         function saveNickname() {
             state.gameData.nickname = allElements.welcome.nicknameText.value
             state.firstLoad = false
@@ -95,8 +105,15 @@
             setActiveWindow('menuWindow')
         }
 
+        // Save nickname on button click and on 'Enter'-keydown
         allElements.welcome.confirmButton.addEventListener('click', saveNickname)
+        allElements.welcome.nicknameText.addEventListener('keydown', function(e) {
+            if (e.key === "Enter") {
+                saveNickname()
+            }
+        })
 
+        // Set correct active window based on firstLoad variable
         if (state.firstLoad) {
             setActiveWindow('welcomeWindow')
         } else {
@@ -104,6 +121,62 @@
             state.startupWindow = 'menuWindow'
             saveState()
         }
+
+
+        // MENU SECTION
+        
+
+
+        // GAME SECTION
+        // All possible questions and their options
+        // Addition functions with 2 random numbers and their sum - check later function
+        const quizItems = {
+            addition: {
+                questionText: "What is the sum of these numbers?"
+            },
+            colors: {
+                questionText: "what is the name of this color?",
+                options: {
+                    red: {
+                        hexValue: "#f23030"
+                    },
+                    blue: {
+                        hexValue: "#3050f2"
+                    },
+                    green: {
+                        hexValue: "#30f243"
+                    },
+                    yellow: {
+                        hexValue: "#f2f230"
+                    },
+                    purple: {
+                        hexValue: "#c530f2"
+                    }
+                }
+            },
+            animals: {
+                questionText: "What's this animal called?",
+                options: {
+                    cat: {
+                        src: "../images/animals/cat.jpg"
+                    },
+                    cow: {
+                        src: "../images/animals/cow.jpg"
+                    },
+                    dog: {
+                        src: "../images/animals/dog.jpg"
+                    },
+                    hamster: {
+                        src: "../images/animals/hamster.jpg"
+                    },
+                    horse: {
+                        src: "../images/animals/horse.jpg"
+                    }
+                }
+            }
+        }
+
+
     })
 })()
 
